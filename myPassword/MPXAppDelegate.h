39b4b157f736d4d860033be9b1b3544cdca0d209
Maxthon Chan <xcvista@me.com>
2016-12-17 17:46:54 +0800

    Committing the bulk of the initial project files.

---
+++ myPassword/MPXAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myPassword
+//
+//  Created by Maxthon Chan on 17/12/2016.
+//  Copyright © 2016 DreamCity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myPassword/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  myPassword
+//
+//  Created by Maxthon Chan on 17/12/2016.
+//  Copyright © 2016 DreamCity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MPXAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MPXAppDelegate class]));
+    }
+}
